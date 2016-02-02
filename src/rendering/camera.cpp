238b294b14e735842306c0ac5ac6f7eeb8a45b17
Anton Konstantinov falceeffect <falceeffected@gmail.com>
2016-02-02 20:23:18 +0300

    Implement Camera constructor

--- src/rendering/camera.cpp
+++ src/rendering/camera.cpp
@@ -1,7 +1,19 @@
 #include "camera.h"
 
-Camera::Camera()
-{
+Camera::Camera() {}
 
+Camera::Camera(Scene* scene, const Vec3& position, const Vec3& screen_center, const Vec3& up, double fovy, int h, int w) {
+    this->scene = scene;
+    this->position = position;
+
+    this->w = (position-screen_center).normalize();
+    this->u = cross(up, this->w).normalize();
+    this->v = cross(this->w, this->u);
+
+    this->height = h;
+    this->width = w;
+    this->ratio = w/h;
+
+    this->th_fovy = tan(fovy/2);
+    this->th_fovx = this->th_fovy * this->ratio;
 }
-
--- src/rendering/camera.h
+++ src/rendering/camera.h
@@ -1,22 +1,24 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <cmath>
 #include "math/math.h"
+#include "rendering/ray.h"
 
 class Scene; // forward declaration to avoid mutual inclusion (camera needs only a pointer to the scene)
 
 class Camera
 {
 private:
-    Vec3 position;
-    Vec3 lookAt;
-    Vec3 up;
-    double fovy;
+    Vec3 position, u, v, w; // (u, v, w) is the camera coordinate frame
+    double th_fovy, th_fovx; // tangents of half field of view x and y
+    int height, width; // image dimensions
+    double ratio; // width over height
     Scene* scene;
 
 public:
     Camera();
-    Camera(Scene* scene, const Vec3& position, const Vec3& lookAt, const Vec3& up, double fovy);
+    Camera(Scene* scene, const Vec3& position, const Vec3& screen_center, const Vec3& up, double fovy, int h, int w);
 };
 
 #endif // CAMERA_H

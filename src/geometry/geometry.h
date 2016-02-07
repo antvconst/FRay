4fab376eedfc0854026820b0c8234377c1df1ec2
Anton Konstantinov falceeffect <falceeffected@gmail.com>
2016-02-07 22:46:05 +0300

    Complete declaration of Geometry abstract class

--- src/geometry/geometry.h
+++ src/geometry/geometry.h
@@ -1,11 +1,31 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 
+#include "math/math.h"
+#include "rendering/ray.h"
+#include "rendering/shading/material.h"
+
+class Geometry; // necessary to define Geometry* inside LocalGeometry
+
+class LocalGeometry
+{
+public:
+    Geometry* geo;
+    Vec4 point;
+    Vec3 normal;
+
+    LocalGeometry(Geometry* geo, const Vec4& point, const Vec3& normal) :
+        geo(geo), point(point), normal(normal) {}
+};
 
 class Geometry
 {
 public:
-    Geometry();
+    Mat4 transform;
+    Material* material;
+
+    Geometry() {}
+    virtual bool intersect(const Ray& r, LocalGeometry& lgeo) = 0;
 };
 
 #endif // GEOMETRY_H

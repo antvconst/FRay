dfc8112e6abad2644e1f961e10ea8189e3c0653d
Anton Konstantinov falceeffect <falceeffected@gmail.com>
2016-02-07 22:44:50 +0300

    Fix invalid type for ray origin

--- src/rendering/ray.h
+++ src/rendering/ray.h
@@ -6,11 +6,11 @@
 class Ray
 {    
 public:
-    Vec3 origin;
+    Vec4 origin;
     Vec3 direction;
 
     Ray() {}
-    Ray(const Vec3& origin, const Vec3& direction) : origin(origin), direction(direction) {}
+    Ray(const Vec4& origin, const Vec3& direction) : origin(origin), direction(direction) {}
 };
 
 #endif // RAY_H
